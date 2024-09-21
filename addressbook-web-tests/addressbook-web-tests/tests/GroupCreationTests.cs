ad19fac2230366b0491b71d5ba34e5b44e2c6075
Knyazev1810 <kniaz1818@yandex.ru>
2024-09-21 23:43:23 +0500

    Создал базовый класс тестовых классов, классы-помощники, базовый класс для помощников, класс-менеджер. Удалил лишние классы и тест.

---
+++ addressbook-web-tests/addressbook-web-tests/tests/GroupCreationTests.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading;
+using NUnit.Framework;
+
+namespace WebAddressbookTests
+{
+    [TestFixture]
+    public class GroupCreationTests : TestBase
+    {
+        [Test]
+        public void GroupCreationTest()
+        {
+            GroupData group = new GroupData("test1");
+            group.Header = "test1";
+            group.Footer = "test1";
+
+            app.Groups.Create(group);
+        }
+        
+        [Test]
+        public void EmptyGroupCreationTest()
+        {
+            GroupData group = new GroupData("");
+            group.Header = "";
+            group.Footer = "";
+
+            app.Groups.Create(group);
+        }
+    }
+}
