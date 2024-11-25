f5a50efea2832acf7da6149a30caa7ad7fcd5d56
Knyazev1810 <kniaz1818@yandex.ru>
2024-11-25 22:52:21 +0500

    Создал новую группу проектов для разработки тестов с помощью Autoit для десктопного приложения. Добавил тесты создания группы и удаления группы.

---
+++ addressbook_tests_autoit/addressbook_tests_autoit/tests/GroupCreationTests.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using NUnit.Framework;
+
+namespace addressbook_tests_autoit
+{
+    [TestFixture]
+    public class GroupCreationTests : TestBase
+    {
+        [Test]
+        public void GroupCreationTest()
+        {
+            List<GroupData> oldGroups = app.Groups.GetGroupList();
+
+            GroupData newGroup = new GroupData()
+            {
+                Name = "test"
+            };
+
+            app.Groups.Add(newGroup);
+
+            List<GroupData> newGroups = app.Groups.GetGroupList();
+
+            Assert.AreEqual(oldGroups.Count + 1, newGroups.Count);
+        }
+    }
+}
---
+++ addressbook_tests_autoit/addressbook_tests_autoit/tests/GroupRemovedTests.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using NUnit.Framework;
+
+namespace addressbook_tests_autoit
+{
+    [TestFixture]
+    public class GroupRemovedTests : TestBase
+    {
+        [Test]
+        public void GroupRemovedTest()
+        {
+            List<GroupData> oldGroups = app.Groups.GetGroupList();
+
+
+            app.Groups.Remove("#0|#0");
+
+            List<GroupData> newGroups = app.Groups.GetGroupList();
+
+            Assert.AreEqual(oldGroups.Count - 1, newGroups.Count);
+        }
+    }
+}
